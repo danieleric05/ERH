6cfa33d514dc3458fb68371f5a743a7561e97f5c
Daniel-Eric Aboussou <aboussoudaniel@gmail.com  git config --global user.name Daniel-Eric>
2026-09-17 14:08:10 +0000

    Convertit error_277/277.blade.php en Tailwind, nettoie le contenu copié-collé
    Ce fallback d'erreur générique (utilisé par post_search/post_search_varaiables)
    affichait en réalité une page "Liste des équipes" copiée-collée avec un
    bouton mort (addForm_equipe()) et deux modales d'équipe greffées sans
    rapport avec une page d'erreur. Simplifié en vraie page d'erreur minimale.

--- resources/views/error_277/277.blade.php
+++ resources/views/error_277/277.blade.php
@@ -1,42 +1,13 @@
 @extends('layouts.erh')
 @section('content')
 
-    <div class="block-header">
-        <div class="row">
-            <div class="col-lg-6 col-md-8 col-sm-12">
-                <h2><a href="javascript:void(0);" class="btn btn-xs btn-link btn-toggle-fullwidth">
-                        <i class="fa fa-arrow-left"></i></a> Liste des équipes </h2>
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ url('bienvenue') }}"><i class="icon-home"></i></a></li>
-                    <li class="breadcrumb-item">Configuration</li>
-                    <li class="breadcrumb-item active">Liste des équipes</li>
-                </ul>
-            </div>
-            <div class="col-lg-6 col-md-4 col-sm-12 text-right">
-
-            </div>
-        </div>
+    <div class="bg-white rounded-lg shadow-lg-soft p-8 text-center">
+        <h1 class="text-2xl font-bold text-text-primary mb-2">Erreur 277</h1>
+        <p class="text-text-secondary mb-6">Une erreur est survenue lors du traitement de votre demande.</p>
+        <a href="{{ url('bienvenue') }}"
+           class="inline-flex items-center gap-2 px-4 py-2 bg-primary-accent text-white font-semibold rounded-lg hover:bg-plastica-blue transition">
+            <i class="fa fa-home"></i> Retour à l'accueil
+        </a>
     </div>
-
-    <div class="row clearfix">
-
-        <div class="col-lg-12">
-            <div class="card">
-
-                <a onclick="addForm_equipe()"  style="float: right; color: #fff;" class="btn btn-danger m-b-15 m-t-10 m-r-20">
-                    <i class="icon wb-plus" aria-hidden="true"></i> Ajouter une équipe
-                </a>
-
-                <div class="body">
-                   <h1>ERROR 277</h1>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    @include('configuration.equipe.modal_equipe')
-    @include('configuration.equipe.edit')
-
 
 @endsection
